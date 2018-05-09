R vs mua works but still dependent on N and I think units are wrong

Rvsmua/main.py
@@ -11,28 +11,31 @@ import matplotlib.pyplot as plt
 import runsimulation as rs
 import numpy as np
 
-def main():
+def main(r):
     
     mualist = []
     Rlist = []
     
-    for mua in np.arange(0.1, 4., 0.1):
+    for mua in np.arange(0.1, 2.5, 0.2):
+        print(mua)
         
         mualist.append(mua)
     
-        prop = rs.runSimulation(mua)
+        prop = rs.runSimulation(mua, r)
         
         Rlist.append(prop.R)
         
-        # Plot of pathlength distribution.
-        plt.figure()
-        plt.hist(prop.pathlengths, bins = 100, weights = prop.weights)
-        plt.title("Pathlength distribution for mu_a = {}".format(mua))
-        plt.xlabel("pathlength (cm or mm ?)")
-        plt.ylabel("amount of photons")
+    # Plot of pathlength distribution.
+    plt.figure()
+    plt.hist(prop.pathlengths, bins = 100, weights = prop.weights)
+    plt.title("Pathlength distribution for mu_a = {}".format(mua))
+    plt.xlabel("pathlength (cm or mm ?)")
+    plt.ylabel("amount of photons")
         
     prop.RvsMua(mualist, Rlist)
     
+    return mualist, Rlist
+    
 if __name__ == "__main__":
     
-    main()
+    main(0.3)

Rvsr/analytical.py
@@ -8,10 +8,10 @@ import math
 import matplotlib.pyplot as plt
 import numpy as np
 
-def main():
+def main(mua):
     """ Generates analytical solution. """
 
-    props = Properties()
+    props = Properties(mua)
     
     # Finding R for each r.
     for r in np.arange(0, 1., 0.002):
@@ -44,10 +44,10 @@ def main():
 class Properties:
     """ Contains all variables. """
     
-    def __init__(self):
+    def __init__(self, mua):
         
         # The tissue properties.
-        self.mua = 1.
+        self.mua = mua
         #self.mus = 10.
         self.g = 0.8
         #self.muc = (1 - self.g) * self.mus
@@ -77,4 +77,5 @@ class Properties:
         
     
 if __name__ == "__main__":
-    main()
+    mua = 1.
+    main(mua)