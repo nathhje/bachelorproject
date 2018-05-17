b0fafb4db10f2dbe51e6b079b15a6fbc60979139
nathhje <n.van.sburg@hotmail.nl>
2018-05-17 16:16:05 +0200

    handy for filtering reflection

---
+++ savephotons/filterreflect.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 17 11:10:54 2018
+
+Filters only the reflected photons, not the absorped.
+"""
+
+import csv
+
+reflectedPhotons = []
+
+with open("actualphotonsformua4.7.csv", 'r') as csvfile:
+    
+    reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+    
+    counter = 0
+    
+    for row in reader:
+        
+        # Checks if row is reflected or absorbed
+        if row[0] == 'r':
+            
+            reflectedPhotons.append(row)
+        counter += 1
+        
+        # Results are written away to avoid memory error
+        if counter % 10000 == 0:
+            
+            with open("photonsformua4.7.csv", 'a', newline='') as csvfile:
+                writer = csv.writer(csvfile, delimiter=',', quotechar='"')
+    
+                for row in reflectedPhotons:
+        
+                    writer.writerow(row)
+                    
+            reflectedPhotons = []
+            
+# Final rows are saved
+with open("photonsformua4.7", 'a', newline='') as csvfile:
+    writer = csv.writer(csvfile, delimiter=',', quotechar='"')
+    
+    for row in reflectedPhotons:
+        
+        writer.writerow(row)
+        
